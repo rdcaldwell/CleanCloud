d4557ee1946c4220e32ade47ea3c331311aa6789
rdcaldwell <rdcald@yahoo.com>
2018-03-11 16:42:17 -0400

    Karma mock backend response

--- src/app/services/amazonweb.service.spec.ts
+++ src/app/services/amazonweb.service.spec.ts
@@ -1,13 +1,20 @@
-import { TestBed, inject } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { AmazonWebService } from './amazonweb.service';
-import { Http } from '@angular/http';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpModule, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
-describe('AmazonWebService', () => {
+fdescribe('AmazonWebService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ FormsModule ],
-      providers: [AmazonWebService]
+      imports: [
+        FormsModule,
+        HttpModule
+      ],
+      providers: [
+        AmazonWebService,
+        { provide: XHRBackend, useClass: MockBackend }
+      ],
     });
   });
 
@@ -15,9 +22,42 @@ describe('AmazonWebService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should be created',  inject([AmazonWebService], (service: AmazonWebService) => {
-    service.getJanitors().subscribe(value => {
-      expect(value).toBe('observable value');
+  it('should get all janitors', inject([AmazonWebService, XHRBackend], (amazonWebService: AmazonWebService, mockBackend: MockBackend) => {
+    const mockResponse = [
+      {
+        '_id': '5aa59136ae0df1dbc214c7bd',
+        'region': 'us-west-2',
+        'defaultEmail': 'cloudianapp@gmail.com',
+        'summaryEmail': 'cloudianapp@gmail.com',
+        'sourceEmail': 'rdcaldwell5705@eagle.fgcu.edu',
+        'isMonkeyTime': true,
+        'port': 3333,
+        '__v': 0
+      },
+      {
+        '_id': '5aa591a8ae0df1dbc214c7be',
+        'region': 'us-east-1',
+        'defaultEmail': 'cloudianapp@gmail.com',
+        'summaryEmail': 'cloudianapp@gmail.com',
+        'sourceEmail': 'rdcaldwell5705@eagle.fgcu.edu',
+        'isMonkeyTime': true,
+        'port': 4444,
+        '__v': 0
+      }
+    ];
+
+    mockBackend.connections.subscribe((connection) => {
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify(mockResponse)
+      })));
+    });
+
+    amazonWebService.getJanitors().subscribe(janitors => {
+      expect(janitors.length).toBe(2);
+      expect(janitors[0].region).toEqual('us-west-2');
+      expect(janitors[0].port).toBe(3333);
+      expect(janitors[1].region).toEqual('us-east-1');
+      expect(janitors[1].port).toBe(4444);
     });
   }));
 });
